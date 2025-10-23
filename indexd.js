afe3cc45f3228b26903210d4489b9230cf1a4b8f
Akram Ansari <akramansari27923mah@gmail.com>
2025-10-23 07:10:39 -0400

    7 commit

---
+++ indexd.js
@@ -0,0 +1,49 @@
+// let p = document.querySelector("p")
+// function click(){
+//     p.style.color = "red"
+// }
+// p.addEventListener("click" ,  click)
+// p.removeEventListener("click" , click)
+
+// let inp = document.querySelector("input")
+// inp.addEventListener("input" , function(dets){
+//     if(dets.data !== null){
+//           console.log(dets.data);
+//     }
+    
+// })
+
+
+// let h1 = document.querySelector("h1")
+// let select = document.querySelector('select')
+// select.addEventListener("change" , function(dets){
+//   h1.textContent = `selected device ${dets.target.value}`
+  
+// })
+
+// let h1 = document.querySelector("h1")
+
+// window.addEventListener("keydown" , function(dets){
+//     if(dets.key === " "){
+//       h1.textContent = "space"
+//     }else{
+//       h1.textContent = `${dets.key}`
+//     }
+  
+// })
+
+// let inp = document.querySelector("#file")
+// document.querySelector(".Uplode").addEventListener("click" , ()=>{
+//   inp.click()
+// })
+
+// let div = document.querySelector(".Uplode")
+
+// inp.addEventListener("change" , function(dets){
+//     const files = dets.target.files[0]
+//     if(files){
+//         files.name    
+//     }
+    
+// })
+
